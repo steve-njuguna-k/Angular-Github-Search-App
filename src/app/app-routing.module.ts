ada4bd244d2fb810d36fee8d1cbc883874c889c3
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-27 12:47:37 +0300

    Added option cards to main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
+  {path:'', component: MainPageComponent},
   {path:'users', component: UsersComponent}
 ];
 
