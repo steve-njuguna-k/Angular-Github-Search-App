07adf14b57e594a9b2d99dd3e35e48c871f9b1e8
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-27 19:40:58 +0300

    Added repo route to links & search bar to repo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { ReposComponent } from './components/repos/repos.component';
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   {path:'', component: MainPageComponent},
-  {path:'users', component: UsersComponent}
+  {path:'users', component: UsersComponent},
+  {path:'repos', component: ReposComponent}
 ];
 
 @NgModule({
