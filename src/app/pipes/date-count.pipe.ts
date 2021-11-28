749b5f81686d8030f40e8a0c726f6070a23e3cf3
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-28 19:16:10 +0300

    Adjusted Date Count pipe

--- src/app/pipes/date-count.pipe.ts
+++ src/app/pipes/date-count.pipe.ts
@@ -5,20 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any): number {
-    let today:Date = new Date();
-    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var dateDifference =Math.abs(todayWithNoTime - value)
-    const secondsInADay= 86400;
+  transform(value: any): number { 
+    let today:Date = new Date(); // get current date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    value = value.split('');
+    let createdYear: any = (value.splice(0,4)).join('');
+    value.shift();
+    let createdMonth: any = ((value.splice(0,2)).join(''))-1;
+    value.shift();
+    let createdDate: any = (value.splice(0,2)).join('');
+    let timeCreated: any = new Date(createdYear , createdMonth, createdDate);
 
-    var dateDifferenceSeconds=dateDifference*0.001; 
-    var dateCounter = dateDifferenceSeconds/secondsInADay;
+    let dateDifference = Math.abs(todayWithNoTime - timeCreated); // returns value in miliseconds
+    const secondsInDay = 86400; // 60 seconds * 60 minutes in an hour * 24 hours in a day
+    let dateDifferenceSeconds = dateDifference*0.001; // converts miliseconds to seconds
+    let dateCounter = Math.floor(dateDifferenceSeconds/secondsInDay);
 
-    if (dateCounter >= 1 && todayWithNoTime > value){
-        return dateCounter;
-    }else{
-        return 0;
+    if (dateCounter >= 1 && todayWithNoTime > timeCreated) {
+      return dateCounter;
     }
+    else {
+      return 0;
+    }
+    
+
   }
 
 }
