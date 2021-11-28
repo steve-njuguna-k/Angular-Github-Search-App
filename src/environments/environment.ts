eb9bc52fe8911617e8f7c670a46bfaed3724fe40
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-28 23:14:31 +0300

    Made changes to environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   clientId: "c789323b20a2d31dc9fa",
   clientSecret: "83e428e5ed1c99fded8ab02686753f74656f9f6b",
   apikey: "be05ad82ef1dec303061648a172836e81613bc78",
