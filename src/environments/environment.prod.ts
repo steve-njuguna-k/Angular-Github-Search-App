d9b02329ab4c7a96e7d1a4109ad3a5df9a3e7849
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-28 23:17:38 +0300

    Added project screenshot

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: "c789323b20a2d31dc9fa",
+  clientSecret: "83e428e5ed1c99fded8ab02686753f74656f9f6b",
+  apikey: "be05ad82ef1dec303061648a172836e81613bc78",
 };
